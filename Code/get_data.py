52c9203d42cbd3a2dc4ee0f4159793aa7e6398ee
Lex Huismans <lexhuismans@outlook.com>
2020-10-30 15:34:15 +0100

    Change variable names

--- Code/get_data.py
+++ Code/get_data.py
@@ -13,26 +13,26 @@ offset = np.arange(number_of_pages)*12
 titles = []
 hrefs = []
 
-zeefdruk_pages_URLs = []
+page_URLs = []
 for page_number in offset:
-    zeefdruk_pages_URLs.append('https://www.kunstveiling.nl/veilingopbrengsten/lijst?technique=silkscreen&offset=' + str(page_number))
+    page_URLs.append('https://www.kunstveiling.nl/veilingopbrengsten/lijst?technique=silkscreen&offset=' + str(page_number))
 
 session = HTMLSession()
 page = 0
-for zeefdruk_page_URL in zeefdruk_pages_URLs:
-    zeefdruk_pages = session.get(zeefdruk_page_URL)
+for URL in page_URLs:
+    items_page = session.get(URL)
 
     # Render JavaScript
-    zeefdruk_pages.html.render() # Als deze niet werkt naar de Python(3) folder gaan en install certificates command runnen
+    items_page.html.render() # Als deze niet werkt naar de Python(3) folder gaan en install certificates command runnen
 
-    soup = BeautifulSoup(zeefdruk_pages.html.html, 'html.parser')
+    soup = BeautifulSoup(items_page.html.html, 'html.parser')
 
     # Vind alle items
     # class='row ng-star-inserted' has all the items
     # All items have id=item#
     items = soup.find_all('li', id=re.compile('^item'))
 
-    # Extract title and link from page
+    # Extract title and link of item
     for item in items:
         title = item.find('h3').find('a', href=True)
         title_text = title.text
