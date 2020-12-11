8e178b7c589836970c2b60699f61ed7c9a2992dd
Lex Huismans <lexhuismans@outlook.com>
2020-12-11 11:33:12 +0100

    Latest code

---
+++ Code/database.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+import get_data as gd
+import re
+
+def get_URLS_titles_database(total_pages, file_name = r'zeefdrukken_data.csv', url_start = 'https://www.kunstveiling.nl/veilingopbrengsten/lijst?technique=silkscreen&offset='):
+    """
+    Get URLS and titles van veiling opbrengsten starting on first page.
+
+    Parameters
+    ----------
+    total_pages : int
+        total amount of pages (starting from first page)
+    file_name : str
+        name of file to save to, has to be .csv file.
+    url_start : str
+        what the URL starts with, should end with 'offset='
+    """
+    for page_start in np.arange(1, total_pages, 10):
+        end_page = page_start+9 # Every 10 pages save data
+        if end_page > total_pages:
+            end_page = total_pages
+        titles, urls = gd.get_items_url(url_start, end_page, 12, past_yields=True, page_start = page_start)
+
+        df = pd.DataFrame()
+
+        df['Title'] = titles
+        df['URL'] = urls
+        print(len(df['Title']))
+        df.set_index('URL', inplace=True)
+
+        with open(file_name, 'a') as f:
+            df.to_csv(f, header=f.tell()==0)
+
+def get_data_info_database(data_path = r'../data/zeefdrukken_data.csv', start = 0, stop = False):
+    """
+    Get additional data of item range of data frame extracted from .csv file.
+
+    Parameters
+    ----------
+    data_path : str
+        path were data is stored.
+    start : int
+        where to start in the data (index).
+    stop : int
+        where to stop in data (index, exlusive). False for end.
+    """
+    items_df = pd.read_csv(data_path)
+    items_df.set_index('URL', inplace=True)
+
+    urls = items_df.index.to_numpy()
+    if stop != False:
+        urls = urls[start:stop]
+    else:
+        urls = urls[start::]
+
+    for url in urls:
+        full_url = 'https://www.kunstveiling.nl' + url
+        item_keys, item_values = gd.get_item_info(full_url)
+
+        items_df.loc[url, item_keys] = item_values
+        # Adding rows: https://stackoverflow.com/questions/45327069/convert-numpy-array-to-pandas-dataframe-column-wise-as-single-row
+        start = start + 1
+    items_df.to_csv(data_path)
+
+def clean_and_save_data(data_path = r'../data/zeefdrukken_data.csv'):
+    df = pd.read_csv(data_path)
+
+    df = gd.clean_data(df)
+
+    new_path = data_path[:-4] + '_cleaned.csv'
+    df.to_csv(new_path)
+    pass
+
+def main(off_set = 6900):
+    for start_at in np.arange(4)*50+off_set:
+        get_data_info(start = start_at, stop = start_at+50)
+        print('item: ', start_at+50)
+
+if __name__ == "__main__":
+    clean_data()
---
+++ Code/ml.py
@@ -0,0 +1,116 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import PoissonRegressor, LinearRegression
+from sklearn.neural_network import MLPRegressor
+from sklearn.experimental import enable_hist_gradient_boosting  # noqa
+from sklearn.ensemble import HistGradientBoostingRegressor
+
+from sklearn import preprocessing
+
+import get_data as gd
+
+"""
+!!TO DO!!
+Make separate function for cleaning data. Maybe clean data right after import. 
+Make separate function for training and saving the result. (pickle)
+"""
+
+columns = ['Price', 'Artist', 'Hoogte', 'Breedte', 'Birth', 'Death', 'Ingelijst', 'Area']
+
+def train_on_data(data_path = r'../data/zeefdrukken_data_cleaned.csv'):
+    df = pd.read_csv(data_path)
+    df['Area'] = df['Hoogte']*df['Breedte']
+
+    df = df[columns].iloc[0:7100,:]
+    df['Death'] = df['Death'].fillna(3000)
+    df.dropna(inplace = True)
+
+    #-------------------------Preprocessing-------------------------------------
+    artist_count = df['Artist'].value_counts()
+    high_freq_artist = artist_count[artist_count > 100].index.tolist()
+    df = df.loc[df['Artist'].isin(high_freq_artist)]
+
+    # Generate binary values using get_dummies
+    dum_df = pd.get_dummies(df['Artist'], columns=['Artist'], prefix=None)
+    df.drop(columns = ['Artist'], inplace=True)
+
+    X = df.iloc[:,1:]
+    y = df.iloc[:,0]
+
+    # Add polynomial features
+    #polyfeatures = preprocessing.PolynomialFeatures(2)
+    #X = polyfeatures.fit_transform(X)
+
+    # Add artists
+    X = np.concatenate((X, dum_df), axis=1)
+    print('Test shape: \n', X.shape)
+
+    #--------------------------------Training-----------------------------------
+    # Split data
+    rng = np.random.RandomState(0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=rng)
+
+    # Scale data
+    scaler = preprocessing.StandardScaler().fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    # Train
+    glm = PoissonRegressor()
+    glm.fit(X_train, y_train)
+    print('Poisson Regression: ', glm.score(X_test, y_test))
+
+    #reg = LinearRegression()
+    #reg.fit(X_train, y_train)
+    #print('Linear Regression: ', reg.score(X_test, y_test))
+
+    reg = MLPRegressor(random_state=1, max_iter=500)
+    reg.fit(X_train, y_train)
+    print('MLP Regressor: ', reg.score(X_test, y_test) )
+
+    #gbdt = HistGradientBoostingRegressor(loss='poisson', learning_rate=.01)
+    #gbdt.fit(X_train, y_train)
+    #print(gbdt.score(X_test, y_test))
+    return reg, high_freq_artist, scaler
+
+def predict_current_offer():
+    """
+    Predict the prizes of the current offer.
+
+    !TO DO!
+    Now have to machine learn every time.
+    Make general place to clean the data.
+    """
+    reg, artists, scaler = train_on_data()
+
+    # Get current offer
+    df = gd.get_current_offer()
+    df = df[columns]
+    print('Current offer: \n', df[['Artist', 'Price']])
+
+    df = df.loc[df['Artist'].isin(artists)]
+    urls = df.index.to_numpy()
+
+    df['Death'] = df['Death'].fillna(3000)
+    df.dropna(inplace = True)
+
+    # Add artist columns
+    df = df.reindex(columns = df.columns.tolist() + artists)
+    for artist in artists:
+        df[[artist]] = df['Artist'].eq(artist)
+
+    df.drop(columns = ['Artist'], inplace=True)
+
+    X = df.iloc[:,1:]
+    print(X)
+    X = scaler.transform(X)
+    print(X)
+    prediction = reg.predict(X)
+
+    current_vs_predict = np.array([[urls], [df.iloc[:,0]], [prediction]])
+    print(current_vs_predict)
+
+if __name__ == '__main__':
+    predict_current_offer()
